087de14c95189359454a96d2faff14a71835bbf3
iganari <cigaguri@gmail.com>
2017-06-30 13:49:38 +0900

    dockeオプションの変更

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,4 @@ set -x
 BASEPATH=$(cd `dirname $0`; pwd)
 REPOPATH=$(cd ../../`dirname $0`; pwd)
 # echo ${REPOPATH}
-docker run --rm -it -v ${BASEPATH}:/var/tmp/project -w /var/tmp/project ${1} /bin/bash
-
+docker run --rm -it -v ${BASEPATH}/src:/go/src -w /go ${1} /bin/bash
