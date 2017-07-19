9786da030849f9cdd0b3c8d7271851df9ef67f46
iganari <cigaguri@gmail.com>
2017-07-19 19:08:26 +0900

    sleepパッケージを入れる

---
+++ src/sleep/sleep-default.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+import (
+       "fmt"
+       "time"
+       )
+*/
+import "time"
+
+func main(){
+        // fmt.Println("This is sleep script")
+        // fmt.iPrintln("10s sleep start")
+        // time.Sleep(ns)
+        time.Sleep(1*time.Second)  // 1秒Sleep
+        // time.Sleep(1*time.Minute)  // 1分Sleep
+        // time.Sleep(1*time.Hour)  // 1時間Sleep
+        // fmt.Println("sleep end")
+}
