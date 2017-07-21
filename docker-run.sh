c745829cbd23275704ec852489be58a3c812c2ea
iganari <cigaguri@gmail.com>
2017-07-21 09:31:01 +0900

    run shellをupdate

--- docker-run.sh
+++ docker-run.sh
@@ -6,4 +6,4 @@ set -x
 BASEPATH=$(cd `dirname $0`; pwd)
 REPOPATH=$(cd ../../`dirname $0`; pwd)
 # echo ${REPOPATH}
-docker run --rm -it -v ${BASEPATH}/src:/go/src -w /go ${1} /bin/bash
+docker run --rm -it -v ${BASEPATH}/src:/go/src -w /go/src ${1} /bin/bash
