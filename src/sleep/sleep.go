92f51aabb3fe1dbbf330a3f3c1bc815ab2264ea9
iganari <cigaguri@gmail.com>
2017-07-21 10:11:13 +0900

    setSleepを関数化

---
+++ src/sleep/sleep.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+       "fmt"
+       "time"
+       )
+
+func setSleep(sleepTime int){
+    fmt.Println("Sleep start :", sleepTime, "s")
+    time.Sleep(time.Duration(sleepTime) * time.Second)
+    fmt.Println("Sleep fnish :", sleepTime, "s")
+}
+
+
+func main(){
+    // fmt.Println("This is sleep script")
+    // var num int = 15
+    // fmt.Println("num", num,  "sec sleep start")
+    // time.Sleep( 1 * time.Second) 
+    // time.Sleep( 1 * time.Minute)
+    // time.Sleep( 1 * time.Hour)
+    // fmt.Println("num", num,  "sec sleep end")
+    setSleep(5)
+}
